4bcd2088ba886c561fe6e11ced660185d1c91728
Alister2600 <145712427+Alister2600@users.noreply.github.com>
2023-10-10 13:36:23 -0500

    Add files via upload

---
+++ Practice with html and css/p5.js/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background (255, 0,0);
+}
+
+function draw() {
+  fill(255, 0, 0);
+  noStroke();
+  rect(100, 100, 200, 200);
+
+  fill(255);
+  triangle(150, 150, 150, 250, 250, 200);
+
+}
